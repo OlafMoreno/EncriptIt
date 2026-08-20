361b949db94c8c66f01e99e59b1b241c7ae6c824
OlafMoreno <160675893+OlafMoreno@users.noreply.github.com>
2026-08-20 12:34:05 +0200

    add: Algorithms options

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -3,14 +3,23 @@ import java.awt.Component;
 import java.awt.Container;
 import java.awt.EventQueue;
 import java.awt.GridLayout;
+import java.io.File;
+import java.nio.file.Files;
+import java.io.IOException;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.border.EmptyBorder;
+
+import Algorithm.AES256;
+import Algorithm.RSA;
+
+import javax.swing.JTextField;
 
 public class MainFrame {
 
@@ -113,10 +122,14 @@ public class MainFrame {
     	 switch (phase) {
 	        case "AlghoritmSelector":
 	            if (aesButton.isSelected()) {
-	                actionLabel.setText("AES256");
+	            	removeMainPanel();
+	                createAesOptionsSelector();
+	                phase="AesOptionsSelector";
 	                return true;
 	            } else if (rsaButton.isSelected()) {
-	                actionLabel.setText("RSA");
+	            	removeMainPanel();
+	                createRsaOptionsSelector();
+	                phase="RsaOptionsSelector";
 	                return true;
 	            } else {
 	                actionLabel.setText("No algorithm selected");
@@ -125,7 +138,8 @@ public class MainFrame {
     	 }
     	 return false;
     }
-    private void removePanel(){
+    
+    private void removeMainPanel(){
     	Container contentPane = mainFrame.getContentPane();
     	BorderLayout layout = (BorderLayout) contentPane.getLayout();
     	Component currentPanel = layout.getLayoutComponent(BorderLayout.CENTER);
@@ -134,5 +148,136 @@ public class MainFrame {
     	}
     	contentPane.revalidate();
         contentPane.repaint();
+    }
+    
+    private void createAesOptionsSelector() {
+        JPanel aes256OptionsSelector = new JPanel();
+
+        aes256OptionsSelector.setBorder(
+            new EmptyBorder(20, 20, 20, 20)
+        );
+
+        aes256OptionsSelector.setLayout(new GridLayout(0, 3, 0, 10));
+
+        mainFrame.getContentPane().add(
+            aes256OptionsSelector,
+            BorderLayout.CENTER
+        );
+
+        JLabel saltLabel = new JLabel("Salt:");
+        aes256OptionsSelector.add(saltLabel);
+        
+        JTextField saltText = new JTextField();
+        aes256OptionsSelector.add(saltText);
+        saltText.setText(AES256.getSalt());
+
+        JButton saltPicker = new JButton("Pick");
+        aes256OptionsSelector.add(saltPicker);
+        
+        saltPicker.addActionListener(e -> {
+        	JFileChooser fileChooser = new JFileChooser();
+        
+            int result = fileChooser.showOpenDialog(mainFrame);
+        
+            if (result == JFileChooser.APPROVE_OPTION) {
+                File file = fileChooser.getSelectedFile();
+
+                try {
+                    String content = Files.readString(file.toPath());
+                    saltText.setText(content);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+          	});
+
+        JLabel secretKeyLabel = new JLabel("Secret Key:");
+        aes256OptionsSelector.add(secretKeyLabel);
+        
+        JTextField secretKeyText = new JTextField();
+        aes256OptionsSelector.add(secretKeyText);
+        secretKeyText.setText(AES256.getSecretKey());
+        JButton secretKeyPicker = new JButton("Pick");
+        aes256OptionsSelector.add(secretKeyPicker);
+        
+        secretKeyPicker.addActionListener(e -> {
+        	JFileChooser fileChooser = new JFileChooser();
+        	int result = fileChooser.showOpenDialog(mainFrame);
+        
+            if (result == JFileChooser.APPROVE_OPTION) {
+            	File file = fileChooser.getSelectedFile();
+                try {
+                	String content = Files.readString(file.toPath());
+                	secretKeyText.setText(content);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }	
+            }
+        });
+    }
+    private void createRsaOptionsSelector() {
+        JPanel rsaOptionsSelector = new JPanel();
+
+        rsaOptionsSelector.setBorder(
+            new EmptyBorder(20, 20, 20, 20)
+        );
+
+        rsaOptionsSelector.setLayout(new GridLayout(0, 3, 0, 10));
+
+        mainFrame.getContentPane().add(
+        	rsaOptionsSelector,
+            BorderLayout.CENTER
+        );
+
+        JLabel publicKeyLabel = new JLabel("PublicKey:");
+        rsaOptionsSelector.add(publicKeyLabel);
+        
+        JTextField publicKeyText = new JTextField();
+        rsaOptionsSelector.add(publicKeyText);
+        publicKeyText.setText(RSA.getPublicKey());
+
+        JButton publicKeyPicker = new JButton("Pick");
+        rsaOptionsSelector.add(publicKeyPicker);
+        
+        publicKeyPicker.addActionListener(e -> {
+        	JFileChooser fileChooser = new JFileChooser();
+        
+            int result = fileChooser.showOpenDialog(mainFrame);
+        
+            if (result == JFileChooser.APPROVE_OPTION) {
+                File file = fileChooser.getSelectedFile();
+
+                try {
+                    String content = Files.readString(file.toPath());
+                    publicKeyText.setText(content);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+          	});
+
+        JLabel privateKeyLabel = new JLabel("Private Key:");
+        rsaOptionsSelector.add(privateKeyLabel);
+        
+        JTextField privateKeyText = new JTextField();
+        rsaOptionsSelector.add(privateKeyText);
+        privateKeyText.setText(RSA.getPrivateKey());
+        JButton privateKeyPicker = new JButton("Pick");
+        rsaOptionsSelector.add(privateKeyPicker);
+        
+        privateKeyPicker.addActionListener(e -> {
+        	JFileChooser fileChooser = new JFileChooser();
+        	int result = fileChooser.showOpenDialog(mainFrame);
+        
+            if (result == JFileChooser.APPROVE_OPTION) {
+            	File file = fileChooser.getSelectedFile();
+                try {
+                	String content = Files.readString(file.toPath());
+                	privateKeyText.setText(content);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }	
+            }
+        });
     }
 }

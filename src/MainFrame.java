e300af20b6205514c0421ecb900ecf0f491968e5
OlafMoreno <160675893+OlafMoreno@users.noreply.github.com>
2026-08-20 12:49:31 +0200

    mod: Change to GridBag layout

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -2,7 +2,6 @@ import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.EventQueue;
-import java.awt.GridLayout;
 import java.io.File;
 import java.nio.file.Files;
 import java.io.IOException;
@@ -23,6 +22,9 @@ import javax.swing.JTextField;
 import java.awt.Button;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 public class MainFrame {
 
@@ -87,19 +89,38 @@ public class MainFrame {
         algorithmSelector.setBorder(
                 new EmptyBorder(20, 20, 20, 20)
             );
-        algorithmSelector.setLayout(new GridLayout(3, 2, 0, 0));
         mainFrame.getContentPane().add(
             algorithmSelector,
             BorderLayout.CENTER
         );
+        GridBagLayout gbl_algorithmSelector = new GridBagLayout();
+        gbl_algorithmSelector.columnWidths = new int[]{314, 0};
+        gbl_algorithmSelector.rowHeights = new int[]{101, 101, 101, 0};
+        gbl_algorithmSelector.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+        gbl_algorithmSelector.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+        algorithmSelector.setLayout(gbl_algorithmSelector);
         JLabel label = new JLabel("Select algorithm:");
-        algorithmSelector.add(label);
-        ButtonGroup group = new ButtonGroup();
+        GridBagConstraints gbc_label = new GridBagConstraints();
+        gbc_label.fill = GridBagConstraints.BOTH;
+        gbc_label.insets = new Insets(0, 0, 5, 0);
+        gbc_label.gridx = 0;
+        gbc_label.gridy = 0;
+        algorithmSelector.add(label, gbc_label);
         aesButton = new JRadioButton("AES256");
-        rsaButton = new JRadioButton("RSA");
-        algorithmSelector.add(aesButton);
-        algorithmSelector.add(rsaButton);
+        GridBagConstraints gbc_aesButton = new GridBagConstraints();
+        gbc_aesButton.fill = GridBagConstraints.BOTH;
+        gbc_aesButton.insets = new Insets(0, 0, 5, 0);
+        gbc_aesButton.gridx = 0;
+        gbc_aesButton.gridy = 1;
+        algorithmSelector.add(aesButton, gbc_aesButton);
+        ButtonGroup group = new ButtonGroup();
         group.add(aesButton);
+        rsaButton = new JRadioButton("RSA");
+        GridBagConstraints gbc_rsaButton = new GridBagConstraints();
+        gbc_rsaButton.fill = GridBagConstraints.BOTH;
+        gbc_rsaButton.gridx = 0;
+        gbc_rsaButton.gridy = 2;
+        algorithmSelector.add(rsaButton, gbc_rsaButton);
         group.add(rsaButton);
 
     }
@@ -179,48 +200,81 @@ public class MainFrame {
             new EmptyBorder(20, 20, 20, 20)
         );
 
-        aes256OptionsSelector.setLayout(new GridLayout(0, 3, 0, 10));
-
         mainFrame.getContentPane().add(
             aes256OptionsSelector,
             BorderLayout.CENTER
         );
-
+        GridBagLayout gbl_aes256OptionsSelector = new GridBagLayout();
+        gbl_aes256OptionsSelector.columnWidths = new int[]{104, 104, 104, 0};
+        gbl_aes256OptionsSelector.rowHeights = new int[]{147, 147, 0};
+        gbl_aes256OptionsSelector.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+        gbl_aes256OptionsSelector.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+        aes256OptionsSelector.setLayout(gbl_aes256OptionsSelector);
+        
         JLabel saltLabel = new JLabel("Salt:");
-        aes256OptionsSelector.add(saltLabel);
+        GridBagConstraints gbc_saltLabel = new GridBagConstraints();
+        gbc_saltLabel.fill = GridBagConstraints.BOTH;
+        gbc_saltLabel.insets = new Insets(0, 0, 5, 5);
+        gbc_saltLabel.gridx = 0;
+        gbc_saltLabel.gridy = 0;
+        aes256OptionsSelector.add(saltLabel, gbc_saltLabel);
         
         JTextField saltText = new JTextField();
-        aes256OptionsSelector.add(saltText);
+        GridBagConstraints gbc_saltText = new GridBagConstraints();
+        gbc_saltText.fill = GridBagConstraints.BOTH;
+        gbc_saltText.insets = new Insets(0, 0, 5, 5);
+        gbc_saltText.gridx = 1;
+        gbc_saltText.gridy = 0;
+        aes256OptionsSelector.add(saltText, gbc_saltText);
         saltText.setText(AES256.getSalt());
-
-        JButton saltPicker = new JButton("Pick");
-        aes256OptionsSelector.add(saltPicker);
         
+        JButton saltPicker = new JButton("Pick");
+        GridBagConstraints gbc_saltPicker = new GridBagConstraints();
+        gbc_saltPicker.fill = GridBagConstraints.BOTH;
+        gbc_saltPicker.insets = new Insets(0, 0, 5, 0);
+        gbc_saltPicker.gridx = 2;
+        gbc_saltPicker.gridy = 0;
+        aes256OptionsSelector.add(saltPicker, gbc_saltPicker);
+
         saltPicker.addActionListener(e -> {
         	JFileChooser fileChooser = new JFileChooser();
-        
-            int result = fileChooser.showOpenDialog(mainFrame);
-        
-            if (result == JFileChooser.APPROVE_OPTION) {
-                File file = fileChooser.getSelectedFile();
 
-                try {
-                    String content = Files.readString(file.toPath());
-                    saltText.setText(content);
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
-            }
-          	});
+        	int result = fileChooser.showOpenDialog(mainFrame);
+
+        	if (result == JFileChooser.APPROVE_OPTION) {
+        		File file = fileChooser.getSelectedFile();
+
+        		try {
+        			String content = Files.readString(file.toPath());
+        			saltText.setText(content);
+        		} catch (IOException ex) {
+        			ex.printStackTrace();
+        		}
+        	}
+        });
 
         JLabel secretKeyLabel = new JLabel("Secret Key:");
-        aes256OptionsSelector.add(secretKeyLabel);
+        GridBagConstraints gbc_secretKeyLabel = new GridBagConstraints();
+        gbc_secretKeyLabel.fill = GridBagConstraints.BOTH;
+        gbc_secretKeyLabel.insets = new Insets(0, 0, 0, 5);
+        gbc_secretKeyLabel.gridx = 0;
+        gbc_secretKeyLabel.gridy = 1;
+        aes256OptionsSelector.add(secretKeyLabel, gbc_secretKeyLabel);
         
         JTextField secretKeyText = new JTextField();
-        aes256OptionsSelector.add(secretKeyText);
+        GridBagConstraints gbc_secretKeyText = new GridBagConstraints();
+        gbc_secretKeyText.fill = GridBagConstraints.BOTH;
+        gbc_secretKeyText.insets = new Insets(0, 0, 0, 5);
+        gbc_secretKeyText.gridx = 1;
+        gbc_secretKeyText.gridy = 1;
+        aes256OptionsSelector.add(secretKeyText, gbc_secretKeyText);
         secretKeyText.setText(AES256.getSecretKey());
         JButton secretKeyPicker = new JButton("Pick");
-        aes256OptionsSelector.add(secretKeyPicker);
+        GridBagConstraints gbc_secretKeyPicker = new GridBagConstraints();
+        gbc_secretKeyPicker.fill = GridBagConstraints.BOTH;
+        gbc_secretKeyPicker.gridx = 2;
+        gbc_secretKeyPicker.gridy = 1;
+        aes256OptionsSelector.add(secretKeyPicker, gbc_secretKeyPicker);
         
         secretKeyPicker.addActionListener(e -> {
         	JFileChooser fileChooser = new JFileChooser();
@@ -244,58 +298,82 @@ public class MainFrame {
             new EmptyBorder(20, 20, 20, 20)
         );
 
-        rsaOptionsSelector.setLayout(new GridLayout(0, 3, 0, 10));
-
         mainFrame.getContentPane().add(
         	rsaOptionsSelector,
             BorderLayout.CENTER
         );
-
+        GridBagLayout gbl_rsaOptionsSelector = new GridBagLayout();
+        gbl_rsaOptionsSelector.columnWidths = new int[]{129, 129, 129, 0};
+        gbl_rsaOptionsSelector.rowHeights = new int[]{94, 94, 94, 0};
+        gbl_rsaOptionsSelector.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+        gbl_rsaOptionsSelector.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+        rsaOptionsSelector.setLayout(gbl_rsaOptionsSelector);
+                
         JLabel publicKeyLabel = new JLabel("PublicKey:");
-        rsaOptionsSelector.add(publicKeyLabel);
-        
+        GridBagConstraints gbc_publicKeyLabel = new GridBagConstraints();
+        gbc_publicKeyLabel.fill = GridBagConstraints.BOTH;
+        gbc_publicKeyLabel.insets = new Insets(0, 0, 5, 5);
+        gbc_publicKeyLabel.gridx = 0;
+        gbc_publicKeyLabel.gridy = 0;
+        rsaOptionsSelector.add(publicKeyLabel, gbc_publicKeyLabel);
+
         JTextField publicKeyText = new JTextField();
-        rsaOptionsSelector.add(publicKeyText);
+        GridBagConstraints gbc_publicKeyText = new GridBagConstraints();
+        gbc_publicKeyText.fill = GridBagConstraints.BOTH;
+        gbc_publicKeyText.insets = new Insets(0, 0, 5, 5);
+        gbc_publicKeyText.gridx = 1;
+        gbc_publicKeyText.gridy = 0;
+        rsaOptionsSelector.add(publicKeyText, gbc_publicKeyText);
         publicKeyText.setText(RSA.getPublicKey());
 
         JButton publicKeyPicker = new JButton("Pick");
-        rsaOptionsSelector.add(publicKeyPicker);
-        
+        GridBagConstraints gbc_publicKeyPicker = new GridBagConstraints();
+        gbc_publicKeyPicker.fill = GridBagConstraints.BOTH;
+        gbc_publicKeyPicker.insets = new Insets(0, 0, 5, 0);
+        gbc_publicKeyPicker.gridx = 2;
+        gbc_publicKeyPicker.gridy = 0;
+        rsaOptionsSelector.add(publicKeyPicker, gbc_publicKeyPicker);
+
         publicKeyPicker.addActionListener(e -> {
         	JFileChooser fileChooser = new JFileChooser();
-        
-            int result = fileChooser.showOpenDialog(mainFrame);
-        
-            if (result == JFileChooser.APPROVE_OPTION) {
-                File file = fileChooser.getSelectedFile();
 
-                try {
-                    String content = Files.readString(file.toPath());
-                    publicKeyText.setText(content);
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
-            }
-          	});
+        	int result = fileChooser.showOpenDialog(mainFrame);
 
-        JLabel privateKeyLabel = new JLabel("Private Key:");
-        rsaOptionsSelector.add(privateKeyLabel);
-        
-        JTextField privateKeyText = new JTextField();
-        rsaOptionsSelector.add(privateKeyText);
-        privateKeyText.setText(RSA.getPrivateKey());
-        JButton privateKeyPicker = new JButton("Pick");
-        rsaOptionsSelector.add(privateKeyPicker);
-        
-        Button buttonGenerateKeys = new Button("Generate Random Keys");
-        buttonGenerateKeys.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-                actionLabel.setText(RSA.generateKeys());
-                publicKeyText.setText(RSA.getPublicKey());
-                privateKeyText.setText(RSA.getPrivateKey());
+        	if (result == JFileChooser.APPROVE_OPTION) {
+        		File file = fileChooser.getSelectedFile();
+
+        		try {
+        			String content = Files.readString(file.toPath());
+        			publicKeyText.setText(content);
+        		} catch (IOException ex) {
+        			ex.printStackTrace();
+        		}
         	}
         });
-        rsaOptionsSelector.add(buttonGenerateKeys);
+        
+        JLabel privateKeyLabel = new JLabel("Private Key:");
+        GridBagConstraints gbc_privateKeyLabel = new GridBagConstraints();
+        gbc_privateKeyLabel.fill = GridBagConstraints.BOTH;
+        gbc_privateKeyLabel.insets = new Insets(0, 0, 5, 5);
+        gbc_privateKeyLabel.gridx = 0;
+        gbc_privateKeyLabel.gridy = 1;
+        rsaOptionsSelector.add(privateKeyLabel, gbc_privateKeyLabel);
+
+        JTextField privateKeyText = new JTextField();
+        GridBagConstraints gbc_privateKeyText = new GridBagConstraints();
+        gbc_privateKeyText.fill = GridBagConstraints.BOTH;
+        gbc_privateKeyText.insets = new Insets(0, 0, 5, 5);
+        gbc_privateKeyText.gridx = 1;
+        gbc_privateKeyText.gridy = 1;
+        rsaOptionsSelector.add(privateKeyText, gbc_privateKeyText);
+        privateKeyText.setText(RSA.getPrivateKey());
+        JButton privateKeyPicker = new JButton("Pick");
+        GridBagConstraints gbc_privateKeyPicker = new GridBagConstraints();
+        gbc_privateKeyPicker.fill = GridBagConstraints.BOTH;
+        gbc_privateKeyPicker.insets = new Insets(0, 0, 5, 0);
+        gbc_privateKeyPicker.gridx = 2;
+        gbc_privateKeyPicker.gridy = 1;
+        rsaOptionsSelector.add(privateKeyPicker, gbc_privateKeyPicker);
         
         privateKeyPicker.addActionListener(e -> {
         	JFileChooser fileChooser = new JFileChooser();
@@ -311,5 +389,20 @@ public class MainFrame {
                 }	
             }
         });
+        
+        Button buttonGenerateKeys = new Button("Generate Random Keys");
+        buttonGenerateKeys.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+                actionLabel.setText(RSA.generateKeys());
+                publicKeyText.setText(RSA.getPublicKey());
+                privateKeyText.setText(RSA.getPrivateKey());
+        	}
+        });
+        GridBagConstraints gbc_buttonGenerateKeys = new GridBagConstraints();
+        gbc_buttonGenerateKeys.fill = GridBagConstraints.BOTH;
+        gbc_buttonGenerateKeys.insets = new Insets(0, 0, 0, 5);
+        gbc_buttonGenerateKeys.gridx = 1;
+        gbc_buttonGenerateKeys.gridy = 2;
+        rsaOptionsSelector.add(buttonGenerateKeys, gbc_buttonGenerateKeys);
     }
 }
